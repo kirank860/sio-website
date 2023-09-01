65afdbc9b019b5fea74e4fecaedcfac0096f3a32
Abhijith ps <abhijithabi22102001@gmail.com>
2023-09-01 17:09:28 +0530

    updated some changes in home page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -441,3 +441,22 @@ $(function () {
     once: true,
   });
 });
+
+// swiper
+var swiper = new Swiper(".c-testimonials", {
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: ".c-testimonials__pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".c-testimonials__arrow-next",
+    prevEl: ".c-testimonials__arrow-prev",
+  },
+});
