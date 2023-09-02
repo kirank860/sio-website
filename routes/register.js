e964fe4277b8e416e13ddc0709170ba9017a0f73
Abhijith ps <abhijithabi22102001@gmail.com>
2023-09-02 17:56:32 +0530

    updates on registration

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,41 @@
 var express = require("express");
 var router = express.Router();
+const Registration = require("../models/Registration");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("register");
+});
+
+router.post("/", async function (req, res, next) {
+  try {
+    console.log(req.body);
+
+    // Check if a user with the same email already exists
+    const existingUser = await Registration.findOne({ email: req.body.email });
+    if (existingUser) {
+      return res.status(400); //.json({ error: "Email already exists" });
+    }
+
+    const newRegistration = new Registration({
+      name: req.body.name,
+      gender: req.body.gender,
+      mobileNumber: req.body.contact,
+      email: req.body.email,
+      district: req.body.location,
+      profession: req.body.profession,
+      regDate: req.body.day,
+      matterOfInterest: req.body.intrest,
+    });
+
+    // Save the registration data to the database
+    await newRegistration.save();
+
+    return res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 module.exports = router;
