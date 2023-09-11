f7b294071adf3243c9db09a83cb314d79f560937
kirank860 <kkiru889@gmail.com>
2023-09-11 11:27:43 +0530

    initial commit

--- routes/news.js
+++ routes/news.js
@@ -3,16 +3,17 @@ const News = require("../models/News");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/:id", async function (req, res, next) {
+router.get("/", async function (req, res, next) {
   try {
-    const id = req.params.id
-    console.log(id)
-    const newsData = await News.findById(id)
-    const news = await News.find()
-    console.log(newsData)
-    res.render("news", { title: "Express", newsData, news });
+    // const id = req.params.id;
+    // console.log(id);
+    const newsData = await News.find();
+    // const news = await News.find();
+    console.log(newsData);
+    // newsData, news
+    res.render("news", { title: "Express", newsData });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 });
 
