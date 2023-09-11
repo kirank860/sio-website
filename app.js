514e64a27e1a40aec068466fd97d198ce7db61f2
Abhijith ps <abhijithabi22102001@gmail.com>
2023-09-11 16:13:34 +0530

    completed deconquista page

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ var Speaker = require("./routes/speaker");
 var Volunter = require("./routes/volunter");
 var Programe = require("./routes/programe");
 var News = require("./routes/news");
+var Deconquista = require("./routes/deconquista");
 
 // ADDED NEWS ROUTES-------
 var Privacy = require("./routes/privacy");
@@ -78,6 +79,7 @@ app.use("/speaker", Speaker);
 app.use("/volunter", Volunter);
 app.use("/programe", Programe);
 app.use("/news", News);
+app.use("/deconquista", Deconquista);
 
 // LATESTS-----
 app.use("/privacy", Privacy);
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -481,3 +481,30 @@ $(".program_count").each(function () {
       }
     );
 });
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  speed: 500,
+  loop: true,
+  centerSlide: true,
+  fade: true,
+  autoplay: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
