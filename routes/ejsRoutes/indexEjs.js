41d3d3ab03133a24d826d39dbbf85269ec1b5a82
Abhijith ps <abhijithabi22102001@gmail.com>
2023-09-12 16:06:33 +0530

    models commented

--- routes/ejsRoutes/indexEjs.js
+++ routes/ejsRoutes/indexEjs.js
@@ -1,33 +1,33 @@
-// const express = require("express");
+const express = require("express");
 // const AboutUs = require("../../models/aboutUs");
 // const Speakers = require("../../models/Speaker");
 // const News = require("../../models/news");
 // const Testimonial = require("../../models/testimonial");
-// const router = express.Router();
+const router = express.Router();
 
-// /* GET home page. */
-// router.get("/", async function (req, res, next) {
-//   try {
-//     const title = "Welcome to My Website";
+/* GET home page. */
+router.get("/", async function (req, res, next) {
+  try {
+    // const title = "Welcome to My Website";
 
-//     const aboutData = await AboutUs.find();
+    // const aboutData = await AboutUs.find();
 
-//     const speakerData = await Speakers.find();
+    // const speakerData = await Speakers.find();
 
-//     const newsData = await News.find();
+    // const newsData = await News.find();
 
-//     const testimonialData = await Testimonial.find();
-//     console.log(testimonialData);
-//     res.render("index", {
-//       title,
-//       aboutData,
-//       speakerData,
-//       newsData,
-//       testimonialData,
-//     });
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// });
+    const testimonialData = await Testimonial.find();
+    console.log(testimonialData);
+    res.render("index", {
+      // title,
+      // aboutData,
+      // speakerData,
+      // newsData,
+      // testimonialData,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
 
-// module.exports = router;
+module.exports = router;
