9848813879c24a79402a56df5acfc8d7d63a3afb
Adhil Akbar <356adhil@gmail.com>
2023-09-12 21:26:58 +0530

    conflicts

--- routes/ejsRoutes/indexEjs.js
+++ routes/ejsRoutes/indexEjs.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Speakers = require("../../models/Speaker");
-const News = require("../../models/news");
+// const News = require("../../models/news");
 const Testimonial = require("../../models/testimonial");
 const router = express.Router();
 
@@ -9,17 +9,16 @@ router.get("/", async function (req, res, next) {
   try {
     const title = "Welcome to My Website";
 
-
     const speakerData = await Speakers.find();
 
-    const newsData = await News.find();
+    // const newsData = await News.find();
 
     const testimonialData = await Testimonial.find();
     console.log(testimonialData);
     res.render("index", {
       title,
       speakerData,
-      newsData,
+      // newsData,
       testimonialData,
     });
   } catch (error) {
