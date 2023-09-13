0a151465521deabfbaaec04d00b4e738693a6bce
kirank860 <kkiru889@gmail.com>
2023-09-13 12:07:17 +0530

    updates.ejs 404 fixed.

--- routes/ejsRoutes/updatesEjs.js
+++ routes/ejsRoutes/updatesEjs.js
@@ -1,10 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
 router.get("/", async function (req, res, next) {
   try {
-    res.render("updates");
+    res.render("updates"); // Sending the response
   } catch (error) {
     console.error(error);
   }
